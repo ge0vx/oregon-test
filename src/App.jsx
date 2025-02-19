dea3e94e6958cbf00123d384a49e86230e9aed4f
ge0vx <giovanypeb@gmail.com>
2025-02-18 21:04:24 -0600

    setting up react-simple-keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from "react";
+import { KeyboardReact } from "react-simple-keyboard";
 import './App.css'
+import "react-simple-keyboard/build/css/index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const keyboard = useRef();
+  const [layout, setLayout] = useState("default");
+
+  const onKeyPress = (button) => {
+    if (button === "{shift}" || button === "{lock}") return handleShift();
+  };
+
+//keyboard's default and lock layouts
+  const keyboardLayout = {
+    default: [
+      "[◂ ▸] 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+      "{tab} q w e r t y u i o p [ ] \\",
+      "{lock} a s d f g h j k l ; ' {enter}",
+      "{shift} z x c v b n m , . / {shift}",
+      ".com @ {space}",
+    ],
+    shift: [
+      "[◂ ▸] ! @ # $ % ^ &amp; * ( ) _ + {bksp}",
+      "{tab} Q W E R T Y U I O P { } |",
+      '{lock} A S D F G H J K L : " {enter}',
+      "{shift} Z X C V B N M &lt; &gt; ? {shift}",
+      ".com @ {space}",
+    ],
+  };
+
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Vite + React!</h1>
+      <div style={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}>
+        <KeyboardReact
+          keyboardRef={keyboard.current}
+          layoutName={layout}
+          layout={keyboardLayout}
+          onKeyPress={onKeyPress}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
