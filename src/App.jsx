3816dc07f0f4f574b5ae058e401d0ebc8da1a55f
ge0vx <giovanypeb@gmail.com>
2025-02-19 00:11:40 -0600

    styling page

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,28 @@
-import { useRef, useState } from "react";
-import { KeyboardReact } from "react-simple-keyboard";
-import './App.css'
 import "react-simple-keyboard/build/css/index.css";
+import { Keyboard } from "./components/Keyboard";
+import { InputText } from "./components/InputText";
 
 function App() {
-  const keyboard = useRef();
-  const [layout, setLayout] = useState("default");
 
-  const onKeyPress = (button) => {
-    if (button === "{shift}" || button === "{lock}") return handleShift();
-  };
-
-//keyboard's default and lock layouts
-  const keyboardLayout = {
-    default: [
-      "[◂ ▸] 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-      "{tab} q w e r t y u i o p [ ] \\",
-      "{lock} a s d f g h j k l ; ' {enter}",
-      "{shift} z x c v b n m , . / {shift}",
-      ".com @ {space}",
-    ],
-    shift: [
-      "[◂ ▸] ! @ # $ % ^ &amp; * ( ) _ + {bksp}",
-      "{tab} Q W E R T Y U I O P { } |",
-      '{lock} A S D F G H J K L : " {enter}',
-      "{shift} Z X C V B N M &lt; &gt; ? {shift}",
-      ".com @ {space}",
-    ],
-  };
-
-  const handleShift = () => {
-    const newLayoutName = layout === "default" ? "shift" : "default";
-    setLayout(newLayoutName);
-  };
 
   return (
-    <>
-      <h1>Vite + React!</h1>
-      <div style={{
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}>
-        <KeyboardReact
-          keyboardRef={keyboard.current}
-          layoutName={layout}
-          layout={keyboardLayout}
-          onKeyPress={onKeyPress}
-        />
+      <div className="block max-w-sm p-10 bg-gray-800 border border-gray-600 rounded-lg shadow-sm card">
+        <h5 className="mb-10 text-2xl font-bold tracking-tight text-white text-center">
+          User Login
+        </h5>
+        <InputText />
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <Keyboard />
+        </div>
       </div>
-    </>
   )
 }
 
-export default App
+export default App;
